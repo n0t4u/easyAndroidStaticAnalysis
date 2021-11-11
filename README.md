291607e21cd61443e5b5f8c369eb6bdff4327b6a
n0t4u <n0t4u@protonmail.com>
2021-11-11 17:09:48 +0100

    Added new commands and minor fixes

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Small script written in bash for decompiling an APK and extracting relevant info
 ## To Do
 - Add the rest of the components
 - Identify exported components
+- Check if exported activities has arguments
 - ...
--- easyAndroidStaticAnalysis.sh
+++ easyAndroidStaticAnalysis.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
 #Author: n0t4u
-#Version: 0.1.2
+#Version: 0.2.0
+
+#Source: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Reset
+ColorOff='\033[0m'       # Text Reset
+
 
 if [ "$#" -lt 1 ]; then
-	echo "No file was provided"
+	echo -e "${Red}[x] No file was provided${ColorOff}"
 	exit 1
 else
-	echo "$1"
+	echo -e "${Cyan}File${ColorOff}\t$1"
 fi
 
 if [ -z '$(which jadx)' ]; then
-	echo "jadx is not installed. Installing now..."
+	echo -e "${Blue}[*] jadx is not installed. Installing now...${ColorOff}"
 	sudo apt install jadx
 	if [ $? -eq 0 ]; then
-		echo "jadx correctly installed. Run the script again."
+		echo -e "${Green}[»] jadx correctly installed. Run the script again.${ColorOff}"
 		exit 0
 	fi
 fi
@@ -24,57 +39,88 @@ else
 	path="$(pwd)"
 fi
 
-# echo "${path}"
-# mkdir -p "${path}/Decompiled"
-# decompile="jadx --no-debug-info -d ${path}/Decompiled $(pwd)/$1"
-# $decompile
-
-if [ $? -ne 0 ]; then
-	echo "There was a problem while trying to decompile the application"
-	exit 1
+echo -e "${Cyan}Path${ColorOff}\t${path}"
+if [[ -d "${path}/Decompiled" ]];then
+    echo -e "${Yellow}[!] Decompiled folder found. Skipping APK decompilation.${ColorOff}"
 else
-	echo "Successfully decompiled"
-	#Find manifest location
-	manifest=$(find ${path}/Decompiled -type f -name AndroidManifest.xml)
-	echo "${manifest}"
-	#Get Package name of the application
-	echo -e "Package name \t $(grep -i -P "package=\"[\S]+\""  ${manifest} -o | awk '{print $2}' FS='=\"' |tr -d '"')"
-	#Get minimum SDK version allowed
-	echo -e "Min SDK\t\t $(grep -i -P "minSdkVersion\=\"[\d]{1,2}" -o  ${manifest} | awk '{print $2}' FS='=\"')"
-	#Get targeted SDK version
-	echo -e "Target SDK\t $(grep -i -P "targetSdkVersion\=\"[\d]{1,2}" -o  ${manifest} | awk '{print $2}' FS='=\"')"
-	#Get maximum SDK version allowed
-	echo -e "Max SDK\t $(grep -i -P "maxSdkVersion\=\"[\d]{1,2}" -o  ${manifest} | awk '{print $2}' FS='=\"')"
-	#Get debuggable flag
-	echo -e "Is debuggable?\t $(grep -i -P "android:debuggable=\"(true|false)"  ${manifest} -o | awk '{print $2}' FS='=\"')"
-	#Get backup flag
-	echo -e "AllowBackup?:\t $(grep -i -P "android:allowBackup=\"(true|false)"  ${manifest} -o | awk '{print $2}' FS='=\"')"
-	#Get HTTP flag
-	echo -e "usesCleartextTraffic? \t $(grep -i -P "android:usesCleartextTraffic=\"(true|false)"  ${manifest} -o | awk '{print $2}' FS='=\"')"
-	#Get Permissions
-	echo -e "Permissions"
-	echo "$(grep -i -P '\"[\S\.]+permission[^\"]+' -o ${manifest} | tr -d '"')"
-	#Get IP Addresses in the code
-	echo -e "IP Addresses"
-	echo "$(grep -r -i -P '(25[0-5]|2[0-4][1-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' -o -H -n ${path}/Decompiled/ --color='always' 2>/dev/null)"
-	#Get web connections in the code
-	echo -e "HTTP, HTTPS and file connections"
-	echo -e "$(grep -r -i -P '(http[s]?|file):\/\/[^\"]+' -h -o ${path}/Decompiled/ 2>/dev/null | grep -v -e 'w3.org' -e 'adobe' | sort| uniq)"
-	#Get port numbers in the code
-	echo -e "Port conections"
-	echo -e "$(grep -r -i -P 'port[_\ \-]{1}[\S\.]*\ ?= ?[\d]{1,5}[ ;]' 2>/dev/null -h ${path}/Decompiled/ --color='always' | grep -i -v -e 'support' -e 'report' )" #Remove --color
-	#Get hardcoded passwords
-	echo -e "Hardcoded passwords"
-	echo -e "$(grep -r -i -P '[\S]*P(ass)?w(or)?d ?=[^;]+' -H -n -o --color='always' 2>/dev/null)"
-	#Get application's activities found in the manifest
-	echo -e "Activities"
-	echo -e "$(grep -i -o -P 'activity[\S ]+android:name=\"[^\"]+' ${manifest} | awk '{print $2}' FS='name=\"')"
-	echo -e "for i in $(grep -i -o -P 'activity[\S ]+android:name=\"[^\"]+' $(find . -name AndroidManifest.xml) | awk '{print $2}' FS='name="'); do; find . -name $(echo ${i} | awk '{print $2}' FS='.')*.java; done"
-	echo -e "Exported Activities"
-	echo -e "$(grep -i -P 'activity[\S ]+exported=\"true\"' ${manifest} | grep -P 'name=\"[^\"]+' -o | sed 's/name=\"//g')"
-	echo -e "Overflow vulnerable functions"
-	echo -e "$(grep -r -e 'strcat' -e 'strcpy' -e 'strncat' -e 'strlcat' -e 'strncpy' -e 'strlcpy' -e 'sprintf' -e 'snprintf' -e ' gets(' -e '\.gets(' -h  -H -n --color='always' ${path}/Decompiled/ 2>/dev/null)"
-	echo -e "Raw SQL queries"
-	echo -e "$(grep -r -i -P '(select [\S]+ from|update [\S]+\delete [\S]+ from|insert [\S]* into)' ${path}/Decompiled/ 2>/dev/null)" #Not tried yet
-
+	echo -e "${Cyan}[*] Decompiling APK with jadx...${ColorOff}"
+	mkdir -p "${path}/Decompiled"
+	decompile="jadx --no-debug-info -d ${path}/Decompiled $(pwd)/$1"
+	$decompile
+	if [ $? -ne 0 ]; then
+	echo -e "${Red}[x] There was a problem while trying to decompile the application${ColorOff}"
+	exit 1
+	else
+		echo -e "${Green}[»] Successfully decompiled${ColorOff}"
+	fi
 fi
+
+
+#Find manifest location
+manifest=$(find ${path}/Decompiled -type f -name AndroidManifest.xml)
+echo -e "${Cyan}Manifest Path\t ${ColorOff}${manifest}"
+#Get Package name of the application
+echo -e "${Cyan}Package name${ColorOff}\t $(grep -i -P "package=\"[\S]+\""  ${manifest} -o | awk '{print $2}' FS='=\"' |tr -d '"')"
+#Get minimum SDK version allowed
+echo -e "${Cyan}Min SDK${ColorOff}\t\t $(grep -i -P "minSdkVersion\=\"[\d]{1,2}" -o  ${manifest} | awk '{print $2}' FS='=\"')"
+#Get targeted SDK version
+echo -e "${Cyan}Target SDK${ColorOff}\t $(grep -i -P "targetSdkVersion\=\"[\d]{1,2}" -o  ${manifest} | awk '{print $2}' FS='=\"')"
+#Get maximum SDK version allowed
+echo -e "${Cyan}Max SDK${ColorOff}\t $(grep -i -P "maxSdkVersion\=\"[\d]{1,2}" -o  ${manifest} | awk '{print $2}' FS='=\"')"
+#Get debuggable flag
+echo -e "${Cyan}Is debuggable?${ColorOff}\t $(grep -i -P "android:debuggable=\"(true|false)"  ${manifest} -o | awk '{print $2}' FS='=\"')"
+#Get backup flag
+echo -e "${Cyan}AllowBackup?${ColorOff}\t $(grep -i -P "android:allowBackup=\"(true|false)"  ${manifest} -o | awk '{print $2}' FS='=\"')"
+#Get HTTP flag
+echo -e "${Cyan}usesCleartextTraffic?${ColorOff}\t $(grep -i -P "android:usesCleartextTraffic=\"(true|false)"  ${manifest} -o | awk '{print $2}' FS='=\"')"
+#Get install location
+echo -e "${Cyan}installLocation${ColorOff}\t $(grep -i -P "android:installLocation=\"[\S]+\"" ${manifest} -o | awk '{print $2}' FS='=\"' | sed 's/\"$//g')"
+
+#Get Permissions
+echo -e "\n${Cyan}Permissions${ColorOff}"
+echo "$(grep -i -P '\"[\S\.]+permission[^\"]+' -o ${manifest} | tr -d '"')"
+#Get IP Addresses in the code
+echo -e "${Cyan}IP Addresses${ColorOff}"
+echo "$(grep -r -i -P '(25[0-5]|2[0-4][1-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' -o -H -n ${path}/Decompiled/ --color='always' 2>/dev/null | grep -v -e '0.0.0.0')" #-H -n
+#Get web connections in the code
+echo -e "${Cyan}HTTP, HTTPS and file connections${ColorOff}"
+echo -e "$(grep -r -i -P '(http[s]?|file):\/\/[^\"]+' -h -o ${path}/Decompiled/ 2>/dev/null | grep -v -e 'w3.org' -e 'adobe' | sort| uniq)"
+#Get port numbers in the code
+echo -e "${Cyan}Port conections${ColorOff}"
+echo -e "$(grep -r -i -P 'port[_\ \-]{1}[\S\.]*\ ?= ?[\d]{1,5}[ ;]' 2>/dev/null -h ${path}/Decompiled/ --color='always' | grep -i -v -e 'support' -e 'report' )" #Remove --color
+#Get hardcoded passwords
+echo -e "${Cyan}Hardcoded passwords${ColorOff}"
+echo -e "$(grep -r -i -P '[\S]*P(ass)?w(or)?d ?=[^;]+' -H -n -o --color='always' 2>/dev/null)"
+#Get application's activities found in the manifest
+activities=$(grep -i -o -P 'activity[\S ]+android:name=\"[^\"]+' ${manifest} | awk '{print $2}' FS='name="')
+echo -e "${Cyan}Activity names${ColorOff}"
+for i in ${activities}; do echo ${i};	done
+echo -e "${Cyan}Activity paths${ColorOff}"
+for i in ${activities}; do find . -name $(echo ${i} | awk '{print $2}' FS='.')*.java;	done
+#Identify exported Activities
+echo -e "${Cyan}Exported activities${ColorOff}"
+for i in $(grep -i -P 'activity[\S ]+exported=\"true\"' ${manifest} | grep -P 'name=\"[^\"]+' -o | sed 's/name=\"//g'); do echo ${i};done
+#Get application's services found in the manifest
+echo -e "${Cyan}Services${ColorOff}"
+#Identify exported Services
+echo -e "${Cyan}Exported services${ColorOff}"
+#Get application's receivers found in the manifest
+echo -e "${Cyan}Receivers${ColorOff}"
+for i in $(grep -i -o -P 'receiver[\S ]+android:name=\"[^\"]+' ${manifest} | awk '{print $2}' FS='name="'); do echo ${i};	done
+#Identify exported Receivers
+echo -e "${Cyan}Exported receivers${ColorOff}"
+#Get application's providers found in the manifest
+echo -e "${Cyan}Providers${ColorOff}"
+for i in $(grep -i -o -P 'provider[\S ]+android:name=\"[^\"]+' ${manifest} | awk '{print $2}' FS='name="'); do echo ${i};	done
+#Identify exported Providers
+echo -e "${Cyan}Exported providers${ColorOff}"
+echo -e "$(grep -i -P 'provider[\S ]+exported=\"true\"' ${manifest} | grep -P 'name=\"[^\"]+' -o | sed 's/name=\"//g')"
+#Check if any overflow vulnerable function is used"
+echo -e "${Cyan}Overflow vulnerable functions${ColorOff}"
+echo -e "$(grep -r -e 'strcat' -e 'strcpy' -e 'strncat' -e 'strlcat' -e 'strncpy' -e 'strlcpy' -e 'sprintf' -e 'snprintf' -e ' gets(' -e '\.gets(' -h  -H -n --color='always' ${path}/Decompiled/ 2>/dev/null)"
+#Get Raw SQL queries from code"
+echo -e "${Cyan}Raw SQL queries${ColorOff}"
+echo -e "$(grep -r -i -P '(select [\S]+ from|update [\S]+\delete [\S]+ from|insert [\S]* into)' ${path}/Decompiled/ 2>/dev/null)" #Not tried yet
+#Get keywords regarding to execution commands
+echo -e "${Cyan}Executable commands${ColorOff}"
+for i in $(grep -r -i -P '[\S]+(sql[^ite]{3}|(^android.)runtime|exec(^utor)|(^android.)shell)[\S]+' -o -h ${path}/Decompiled/ 2>/dev/null);do echo -e "${i}"; done
